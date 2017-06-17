c39d09040c48f3d92874c294528021f0c6cff165
karafede <karafede@hotmail.com>
2017-06-17 22:01:31 +0400

    Add files via upload

---
+++ MODIS_download_10K_hdf.R
@@ -0,0 +1,69 @@
+
+library(RCurl,lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(stringr, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(plyr, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(dplyr, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(threadr, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(gdalUtils, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(rgdal, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(raster, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(RNetCDF, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+library(readr, lib.loc="/home/fkaragulian/R/x86_64-pc-linux-gnu-library/3.3")
+
+setwd("/disk3/fkaragulian/MODIS_AOD/")
+wd <- getwd()
+Sys.time() 
+current_date <- str_sub(Sys.time(), start = 1, end = -10)
+str(current_date)
+
+## 3km
+# url_list_data = "ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_3K/2016/"
+## 10km
+url_list_data = "ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_L2/2016/" 
+
+list_data = getURL(url_list_data, ftp.use.epsv = FALSE, ftplistonly = TRUE, crlf = TRUE, ssl.verifypeer = FALSE) 
+list_data = paste(url_list_data, strsplit(list_data, "\r*\n")[[1]], sep = "") 
+# last element (date) of the list in order of time 
+list_data <- sort(list_data)
+n <- as.numeric(length(list_data))
+list_data[n]
+folder <- unlist(list_data[n])
+# isolate folder name
+folder_year <- str_sub(folder, start = 74, end = -5)
+folder_day <- str_sub(folder, start = 79, end = -1) 
+
+# url = paste("ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_3K/2016/",folder_day, "/", sep = "")
+# url = paste("ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_3K/2016/","349", "/", sep = "")
+
+url = paste("ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_L2/2016/",folder_day, "/", sep = "")
+# url = paste("ftp://karafede:Password08@nrt3.modaps.eosdis.nasa.gov/allData/6/MOD04_L2/2016/","350", "/", sep = "")
+
+
+filenames = getURL(url, ftp.use.epsv = FALSE, ftplistonly = TRUE, crlf = TRUE, ssl.verifypeer = FALSE) 
+filenames = paste(url, strsplit(filenames, "\r*\n")[[1]], sep = "") 
+
+# select only files in the time range as in the Overpass time
+
+Overpass_times <- read.csv("/disk3/fkaragulian/MODIS_AOD/Overpass_times.csv")
+
+matches <- unique (grep(paste(Overpass_times$x,collapse="|"), 
+                        filenames, value=TRUE))
+
+
+# create a new working directory for each download date
+dir.create(folder_year)
+# setwd(folder)
+
+# download data
+setwd(paste0(wd,"/",folder_year))
+dir.create(folder_day)
+setwd(paste0(wd,"/",folder_year,"/", folder_day))
+
+filenames_MODIS_10k <- unlist(str_extract_all(matches, ".+(.hdf$)"))
+filenames_MODIS_10k <- sort(filenames_MODIS_10k)
+
+# start downloading data in the main directory -----------------------
+mapply(download.file, filenames_MODIS_10k,basename(filenames_MODIS_10k)) 
+
+
+
