1fd5a68c373a163f0756477083404681700c5ad2
karafede <karafede@hotmail.com>
2017-08-10 09:26:35 +0400

    Add files via upload

--- ECMWF_forecasts_12gmt.sh
+++ ECMWF_forecasts_12gmt.sh
@@ -4,7 +4,7 @@
 mainPath=/home/fkaragulian/ECMWF_forecasts/ ; dest=/research/cesam/AirQuality/forecasts_ECMWF/
 source /home/fkaragulian/export.sh
 
-date=`date +%Y%m%d` ; hr=$1 ; date_1=${date}${hr}
+date=`date -d"-1 day" +%Y%m%d` ; hr=12 ; date_1=${date}${hr}
 #date=$2 ; hr=$1 ; date_1=${date}${hr}
 cd $mainPath
 
@@ -18,9 +18,11 @@ cd $mainPath
 
 mkdir -p ${dest}/${date_1}/ ; cd ${dest}/${date_1}/ ; 
 
-cp -r ${mainPath}/forecasts_downloads/${date_1}/PM25/*.tif .
+# cp -r ${mainPath}/forecasts_downloads/${date_1}/PM25/*.tif .
 
 scp ${mainPath}/forecasts_downloads/${date_1}/PM25/*.tif fkaragulian@10.102.14.39:/home/fkaragulian/mtg/_SHARED_FOLDERS/AQ_Website/ECMWF_forecasts_2017/PM25/
+
+rsync -avz ${mainPath}/forecasts_downloads/${date_1}/PM25/*.tif pvernier@atlas-prod.minet.ae:/home/pvernier/scripts_cron/forecast_data/
 
 echo "fine"
 
